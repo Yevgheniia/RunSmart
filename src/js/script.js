d0591e60c01077545c21311885bb8fb0d8c68ceb
Yevgheniia <etupik13@gmail.com>
2022-04-01 20:18:05 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-alert(1);
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+            items:1,
+            margin:10,
+            nav:true,
+            navText : ['<img src="icons/ArrowPrev.png" alt="ArrowPrev">','<img src="icons/ArrowLast.png" alt="ArrowLast">'],
+            responsive:{
+                0:{
+                    nav:false
+                },
+                732:{
+                    nav:false
+                },
+                1000:{
+                    items:1
+                }
+            }
+        }
+    );
+  });
+  
